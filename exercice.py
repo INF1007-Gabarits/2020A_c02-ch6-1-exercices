ad40e1d4ee62a81b10d75d7cee2e131a06c2e63c
chosson <>
2020-09-28 11:29:46 -0400

    Added code.

--- exercice.py
+++ exercice.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 import math
+import copy
+import itertools
 
-def get_num_letters(text):
-	return 0
 
-def get_word_length_histogram(text):
-	return [0]
+def get_maximums(numbers):
+	return [max(elem) for elem in numbers]
 
-def format_histogram(histogram):
-	ROW_CHAR = "*"
+def join_integers(numbers):
+	return int("".join([str(elem) for elem in numbers]))
 
-	return ""
+def generate_prime_numbers(limit):
+	primes = []
+	numbers = [i for i in range(2, limit)]
+	while len(numbers) != 0:
+		primes.append(numbers[0])
+		numbers = [elem for elem in numbers if elem % numbers[0] != 0]
+	return primes
 
-def format_horizontal_histogram(histogram):
-	BLOCK_CHAR = "|"
-	LINE_CHAR = "¯"
-
-	return ""
-
+def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
+	return [
+		elem + str(i)
+			for i in range(1, num_combinations + 1)
+			if excluded_multiples is None or i % excluded_multiples != 0
+				for elem in strings
+	]
 
 if __name__ == "__main__":
-	spam = "Stop right there criminal scum! shouted the guard confidently."
-	eggs = get_word_length_histogram(spam)
-	print(eggs, "\n")
-	print(format_histogram(eggs), "\n")
-	print(format_horizontal_histogram(eggs))
+	print(get_maximums([[1,2,3], [6,5,4], [10,11,12], [8,9,7]]))
+	print("")
+	print(join_integers([111, 222, 333]))
+	print(join_integers([69, 420]))
+	print("")
+	print(generate_prime_numbers(15))
+	print("")
+	print(combine_strings_and_numbers(["A", "B"], 2, None))
+	print(combine_strings_and_numbers(["A", "B"], 5, 2))

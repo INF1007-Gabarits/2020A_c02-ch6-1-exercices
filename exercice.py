ff4f09149c855a7d56aa51a014a7f8ec0e54854d
chosson <>
2020-09-28 11:55:21 -0400

    Removed answers.

--- exercice.py
+++ exercice.py
@@ -8,26 +8,16 @@ import itertools
 
 
 def get_maximums(numbers):
-	return [max(elem) for elem in numbers]
+	return [0]
 
 def join_integers(numbers):
-	return int("".join([str(elem) for elem in numbers]))
+	return 0
 
 def generate_prime_numbers(limit):
-	primes = []
-	numbers = [i for i in range(2, limit+1)]
-	while len(numbers) != 0:
-		primes.append(numbers[0])
-		numbers = [elem for elem in numbers if elem % numbers[0] != 0]
-	return primes
+	return [0]
 
 def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
-	return [
-		elem + str(i)
-			for i in range(1, num_combinations + 1)
-			if excluded_multiples is None or i % excluded_multiples != 0
-				for elem in strings
-	]
+	return [""]
 
 if __name__ == "__main__":
 	print(get_maximums([[1,2,3], [6,5,4], [10,11,12], [8,9,7]]))
